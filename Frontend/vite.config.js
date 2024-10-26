001ccde2fec463644ee95dc26c8ac3921c3c6c01
dev-kant-kumar <dev.mytechhandle@gmail.com>
2024-10-26 09:56:21 +0530

    done

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0", // Allows access from other devices on the network
+    port: 5173, // Port number, make sure it matches where your app is running
+  },
+});
